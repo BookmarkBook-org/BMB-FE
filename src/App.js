42b3c2c8ffddc392a58908ddfea3308305912118
jrary <jrary@naver.com>
2023-10-04 04:48:28 +0900

    [Feat] header menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import Header from "./components/common/Header";
+
+import Home from './components/pages/Home';
+import Mypage from './components/pages/Mypage';
+import Setting from './components/pages/Setting';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/mypage' element={<Mypage />} />
+        <Route path="/setting" element={<Setting />} />
+      </Routes>
+    </Router>
   );
 }
 
