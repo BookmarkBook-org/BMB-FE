9c9b9ccd04c65253b76d20822e04031e4193d03e
jrary <jrary@naver.com>
2023-10-04 08:33:52 +0900

    [Feat]Dropdown menu 구성

--- src/components/pages/Mypage.js
+++ src/components/pages/Mypage.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Title from "./mypage/Title";
+import UserInfo from "./mypage/UserInfo";
+import BookmarkMenu from "./mypage/BookmarkMenu";
+import BookmarkFolder from "./mypage/BookmarkFolder";
+import BookmarkList from "./mypage/BookmarkList";
 
 const Mypage = () => {
+    const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
     return (
-        <>
-        마이페이지
-        </>
+        <div>
+            <Title />
+            <UserInfo />
+            <BookmarkMenu />
+            <BookmarkFolder />
+            <BookmarkList />
+        </div>
     );
 };
 
---
+++ src/components/pages/mypage/BookmarkMenu.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BookmarkMenu = () => {
+    return (
+        <div>
+          <p>전체 북마크</p>
+          <div>
+            <button>북마크 추가하기</button>
+            <button>폴더 추가하기</button>
+          </div>
+        </div>
+    );
+};
+
+export default BookmarkMenu;
