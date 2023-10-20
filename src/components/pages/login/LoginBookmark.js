5804226e3af187113ca5f446a8679734987be04a
jrary <jrary@naver.com>
2023-10-20 10:00:00 +0900

    로그인 페이지 부분 완성

--- src/components/pages/login/LoginBookmark.js
+++ src/components/pages/login/LoginBookmark.js
@@ -18,14 +18,13 @@ const LoginBookmark = () => {
     }
   }, []); 
 
+  // Dropzone 설정 함수들
   const onDrop = (acceptedFiles) => {
     setUploadedFiles(acceptedFiles);
   };
-
   const resetUploadedFiles = () => {
     setUploadedFiles([]); 
   }
-  
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
     noKeyboard: true, // 키보드로 업로드할 수 없도록 설정
@@ -42,26 +41,46 @@ const LoginBookmark = () => {
       </div>
       <div className="upload-wrapper" >
         <p className="upload-intro">그동안 모아둔 북마크 목록을 <br />다운받은 후, 업로드해주세요.</p>
-        <div className="upload" {...getRootProps()}>
+        <div {...getRootProps()}>
           <input multiple="" type="file" {...getInputProps()} />
-          <img
-            className="img-upload"
-            src="/assets/images/ic_upload.png"
-            alt="logoImg" />
-          <p className="text-upload-1">Click to upload</p>
-          <p className="text-upload-2">or drag and drop</p>
-          <p className="text-upload-3">html파일만 업로드 가능합니다.</p>
+          {uploadedFiles.length > 0 ? (
+            <div className="upload-files">
+            <ul>
+              {uploadedFiles.map((file, index) => (
+                <li className="upload-list" key={index}>
+                  <img 
+                    src="/assets/images/ic_wrapper.svg"
+                  />
+                  {file.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+          ) : (
+            <div className="upload">
+              <img
+                className="img-upload"
+                src="/assets/images/ic_upload.png"
+                alt="logoImg" />
+              <p className="text-upload-1">Click to upload</p>
+              <p className="text-upload-2">or drag and drop</p>
+              <p className="text-upload-3">html파일만 업로드 가능합니다.</p>
+            </div>
+          )}
         </div>
-        <button onClick={resetUploadedFiles}>초기화</button>
-        <button onClick={onNextPage}>업로드</button>
         <div>
-          <p>업로드된 파일 목록:</p>
-          <ul>
-            {uploadedFiles.map((file, index) => (
-              <li key={index}>{file.name}</li>
-            ))}
-          </ul>
+          <button className="btn-reset" onClick={resetUploadedFiles}>초기화</button>
+          <button className="btn-upload" onClick={onNextPage}>업로드</button>
         </div>
+        <div className="div-info">
+          <p className="div-info-title">내 북마크 다운받는 법</p>
+          <ol className="div-info-cont">
+            <li>인터넷 창에서 북마크 바 우클릭</li>		
+            <li>[북마크 관리자] 클릭</li>
+            <li>[북마크 내보내기] 클릭 후, html로 저장</li>
+          </ol>
+        </div>
+        <p className="div-info-more">* 공개하고 싶지 않은 북마크는 북마크북에서 비공개 설정이 가능합니다.</p>
       </div>
     </Wrapper>
   );
@@ -94,7 +113,6 @@ const Wrapper = styled.div`
 .upload {
   background-color: #ffffff; 
   border: 1px dashed #cccccc;
-  max-width: 100%;
   width: 311px;
   height: 132px;
   color: #6c757d;
@@ -106,6 +124,29 @@ const Wrapper = styled.div`
   align-items: center; 
   padding: 24px;
 }
+.upload-files {
+  background-color: #ffffff; 
+  border: 1px dashed #cccccc;
+  max-width: 100%;
+  width: 311px;
+  height: 132px;
+  color: #6c757d;
+
+  margin: 0 auto; 
+  display: flex;
+  flex-direction: column; 
+  list-style: none;
+  padding: 24px;
+}
+.upload-list{
+  color: black;
+  font-size: 16px;
+  font-family: 'Pret-reg';
+  font-weight: 400;
+  list-style: none;
+  line-height: 2px;
+  word-wrap: break-word;
+}
 .img-upload{
   width: 40px;
   height: 40px;
@@ -113,7 +154,7 @@ const Wrapper = styled.div`
 .text-upload-1{
   color: #7749F8;
   font-size: 20px;
-  font-family: Pretendard Variable;
+  font-family: 'Pret-Bold';
   font-weight: 700;
   line-height: 1px;
   word-wrap: break-word
@@ -121,7 +162,7 @@ const Wrapper = styled.div`
 .text-upload-2{
   color: #212529;
   font-size: 20px;
-  font-family: Pretendard Variable;
+  font-family: 'Pret-Bold';
   font-weight: 700;
   line-height: 1px;
   word-wrap: break-word
@@ -129,9 +170,92 @@ const Wrapper = styled.div`
 .text-upload-3{
   color: #6C757D;
   font-size: 14px;
-  font-family: Pretendard;
+  font-family: 'Pret-reg';
   font-weight: 400;
   line-height: 0px;
+  word-wrap: break-word
+}
+.btn-reset{ 
+  width: 65px; 
+  height: 100%; 
+  padding-left: 8px;
+  padding-right: 8px; 
+  padding-top: 10px; 
+  padding-bottom: 10px; 
+  background: white; 
+  border-radius: 8px; 
+  border: 1px #7749F8 solid; 
+  justify-content: center; 
+  align-items: center; 
+  gap: 4px; 
+  display: inline-flex;
+  margin: 8px;
+  margin-left: 0;
+  
+  color: #5227CC;
+  font-size: 16px;
+  font-family: 'Pret-reg';
+  font-weight: 700;
+  line-height: 24px;
+  word-wrap: break-word
+}
+.btn-upload{
+  width: 238px; 
+  height: 100%; 
+  padding-left: 12px; 
+  padding-right: 12px; 
+  padding-top: 10px; 
+  padding-bottom: 10px; 
+  background: #7749F8; 
+  border-radius: 8px; 
+  border: 1px #7749F8 solid; 
+  justify-content: center; 
+  align-items: center; 
+  gap: 4px; 
+  display: inline-flex;
+  margin: 8px;
+
+  color: white;
+  font-size: 16px;
+  font-family: 'Pret-reg';
+  font-weight: 700;
+  line-height: 24px;
+  word-wrap: break-word
+}
+.div-info{
+  width: 100%; 
+  height: 100%; 
+  background: #F8F9FA; 
+  border-radius: 20px; 
+  flex-direction: column; 
+  justify-content: flex-start; 
+  align-items: flex-start; 
+  gap: 1px; 
+  padding: 8px;
+}
+.div-info-title{
+  color: #212529;
+  font-size: 20px;
+  font-family: 'Pret-Bold';
+  font-weight: 700;
+  line-height: 28px;
+  word-wrap: break-word;
+  padding-left: 8px;
+}
+.div-info-cont{
+  color: #212529;
+  font-size: 16px;
+  font-family: 'Pret-reg';
+  font-weight: 400;
+  line-height: 30px;
+  word-wrap: break-word
+}
+.div-info-more{
+  color: #212529;
+  font-size: 14px;
+  font-family: 'Pret-reg';
+  font-weight: 400;
+  line-height: 20px;
   word-wrap: break-word
 }
 .progress-bar {
